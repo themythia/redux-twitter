17df147d3335486460d445da9005491ae5e3af8c
Emir AL <36728456+themythia@users.noreply.github.com>
2021-06-23 11:31:29 +0300

    minor changes to Tweet.js logic

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -2,19 +2,10 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { FaReply, FaHeart } from 'react-icons/fa';
 
-const Tweet = ({
-  id,
-  text,
-  author,
-  timestamp,
-  likes,
-  replies,
-  replyingTo,
-  avatarURL,
-  name,
-}) => {
+const Tweet = ({ id }) => {
   const tweets = useSelector((store) => store.tweets);
   const users = useSelector((store) => store.users);
+  const tweet = tweets[id];
 
   const getDate = (num) => {
     const dateObj = new Date(num);
@@ -23,38 +14,35 @@ const Tweet = ({
 
   return (
     <div className='tweet'>
-      <img src={avatarURL} alt='Profile Picture' className='avatar' />
+      <img
+        src={users[tweet.author].avatarURL}
+        alt='Avatar'
+        className='avatar'
+      />
       <div className='tweet-right'>
-        <p className='tweet-name'>{name}</p>
-        <p className='tweet-date text-faded'>{getDate(timestamp)}</p>
-        {replyingTo !== null && (
-          <p className='tweet-replyingTo text-faded'>Replying to @{author}</p>
+        <p className='tweet-name'>{users[tweet.author].name}</p>
+        <p className='tweet-date text-faded'>{getDate(tweet.timestamp)}</p>
+        {tweet.replyingTo !== null && (
+          <p className='tweet-replyingTo text-faded'>
+            Replying to @{tweet.author}
+          </p>
         )}
-        <p className='tweet-text'>{text}</p>
+        <p className='tweet-text'>{tweet.text}</p>
         <div className='tweet-right-bottom'>
           <div className='tweet-replies'>
             <span>
               <FaReply />
             </span>
-            <span>{replies.length}</span>
+            <span>{tweet.replies.length}</span>
           </div>
           <div className='tweet-likes'>
             <span>
               <FaHeart />
             </span>
-            <span>{likes.length}</span>
+            <span>{tweet.likes.length}</span>
           </div>
         </div>
       </div>
-
-      {/* <p>id: {id}</p>
-      <p>name: {name}</p>
-      <p>{text}</p>
-      <p>author: {author}</p>
-      <p>timestamp: {timestamp}</p>
-      <p>likes: {likes}</p>
-      <p>replies: {replies}</p>
-      <p>replyingTo: {replyingTo}</p> */}
     </div>
   );
 };
--- src/components/Tweets.js
+++ src/components/Tweets.js
@@ -4,29 +4,18 @@ import Tweet from './Tweet';
 const Tweets = () => {
   const users = useSelector((store) => store.users);
   const tweets = useSelector((store) => store.tweets);
-  const usersArray = Object.keys(users).map((username) => users[username]);
+
+  //Converts tweets object into an array and sorts them by timestamp
   const tweetsArray = Object.keys(tweets)
     .map((tweet) => tweets[tweet])
     .sort((a, b) => b.timestamp - a.timestamp);
-  console.log('users', users);
-  console.log('tweetsArray', tweetsArray);
 
   return (
     <div>
       {Object.keys(users).length !== 0 &&
         tweetsArray.map((tweet, index) => (
           <li key={index}>
-            <Tweet
-              id={tweet.id}
-              text={tweet.text}
-              author={tweet.author}
-              timestamp={tweet.timestamp}
-              likes={tweet.likes}
-              replies={tweet.replies}
-              replyingTo={tweet.replyingTo}
-              avatarURL={users[tweet.author].avatarURL}
-              name={users[tweet.author].name}
-            />
+            <Tweet id={tweet.id} />
           </li>
         ))}
     </div>
