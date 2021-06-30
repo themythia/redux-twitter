ffb840449d1acfdab8e3158e881731fa8ce0b109
Emir AL <36728456+themythia@users.noreply.github.com>
2021-06-30 11:46:16 +0300

    minor fixes

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { handleAddTweet } from '../actions/tweets';
+import { Redirect } from 'react-router-dom';
 
 const NewTweet = ({ id }) => {
   const [input, setInput] = React.useState('');
   const dispatch = useDispatch();
+  const [redirectStatus, setRedirectStatus] = React.useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatch(handleAddTweet(input, id));
     setInput('');
+    console.log('id', id);
+    setRedirectStatus(id ? false : true);
   };
+
+  if (redirectStatus) {
+    return <Redirect to='/' />;
+  }
 
   return (
     <React.Fragment>
@@ -20,7 +28,7 @@ const NewTweet = ({ id }) => {
           onChange={(e) => setInput(e.target.value)}
           placeholder={`What's happening?`}
         />
-        <button class='submit-button' type='submit'>
+        <button className='submit-button' type='submit'>
           Submit
         </button>
       </form>
